5884e7e94ff4ad76d998cf40f93cdea424b5077c
Jakub Tucholski <jtucholski00@gmail.com>
2016-06-21 17:01:49 -0700

    working on that cli

--- cli.go
+++ cli.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 import "flag"
 import yaml "gopkg.in/yaml.v2"
+import "os"
+import "log"
+
+type Config struct {
+	DevKey  string `yaml:"devkey"`
+	UserKey string `yaml:"userkey"`
+}
 
 func main() {
 	// setting up flags
@@ -14,4 +21,22 @@ func main() {
 		homeDir := os.Getenv("HOME")
 		*conf = strings.Join([]string{homedir, ".pastebin.yaml"})
 	}
+}
+
+func LoadConfig(confpath string) Config {
+	if _, err := os.Stat(confpath); os.IsNotExist(err) {
+		log.Fatal("Config at '%s' does not exist", confpath)
+	}
+
+	configContents, err := ioutil.ReadFile(confpath)
+	if err != nil {
+		log.Fatal("Could not read config at '%s'", confpath)
+	}
+
+	var config Config
+	err = yaml.Unmarshal(configContents, &config)
+	if err != nil {
+		log.Fatal("Could not unmarshal configuration file")
+	}
+	return config
 }
